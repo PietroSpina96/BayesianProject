65e62595e7027034bfd4fab9d2a0f7d4d29046a7
GiuliaRomani <giulia.romani@mail.polimi.it>
2022-01-04 13:22:31 +0100

    functions_workspace and no functions

--- alpha_Mahalanobis.R
+++ alpha_Mahalanobis.R
@@ -10,6 +10,7 @@ setwd('C:/Users/pietr/Desktop/Bayesian Statistics/Progetto/dati/potenziali_evoca
 setwd("C:/Users/admin/Documents/R/Project_BS/BayesianProject") #GiuliaR
 
 load('functional_WP.RData')
+load('Functions_WP.RData')
 
 
 #### Kernel estimator ####
@@ -29,32 +30,6 @@ for(i in 1:26){
 lines(f.data$ausxSL$argvals, X_bar, type = 'l', lwd=3, col = 'firebrick2')
 
 
-# # Covariance function estimator K_hat
-# kernel_estimator <- function(process,med,n,s,t,points){
-#   est <- rep(0,points)
-#   for (i in 1:n) {
-#     est[i] <- (process[i,s] - med[s])*(process[i,t] - med[t])
-#   }
-#   estimator = (1/n)*sum(est)
-#   
-#   return(estimator)
-# }
-# 
-# # prova
-# kernel_estimator(f.data$ausxSL$data,X_bar,n,1,9,t_points) 
-# 
-# # Covariance matrix for the data
-# K_hat <- matrix(0,t_points,t_points)
-# for (i in 1:t_points){
-#   for (j in 1:t_points){
-#     K_hat[i,j] <- kernel_estimator(f.data$ausxSL$data,X_bar,n,i,j,t_points)
-#   }
-# }
-# # sub-matrix (5x5)
-# khat_example<-K_hat[1:5,1:5]
-# show(khat_example)
-
-
 # Covariance matrix for the data: K_hat_R is the covariance matrix computed through the R function cov
 K_hat_R <- cov(f.data$ausxSL$data) 
 
@@ -67,26 +42,6 @@ x11()
 par(mfrow=c(1,2))
 image.plot(time,time,K_hat,main='Our cov function')
 image.plot(time,time,K_hat_R,main='R cov function')
-
-
-#### alpha-Mahalanobis distance and other usefull functions ####
-# functions
-scalar_prod<- function (f1,f2) {
-  # f sono vettori colonna
-  res<- t(f1)%*%f2
-  return(res)
-}
-
-
-# equivalentemennte
-#scalar_prod_norm<- function (f1,f2) {
-# f sono vettori colonna
-#f1<-as.matrix(f1)
-#f2<-as.matrix(f2)
-#res<- (norm(f1+f2,'2')^2 - norm(f1-f2,'2')^2)/4
-#return(res)
-#}
-
 
 # Compute eigenvalues and eigenfunctions of the covariance matrix
 # eigenvf<-eigen(K_hat)
@@ -103,69 +58,6 @@ eigenft<-eigenvf$vectors
 # statistical units. Here we have N=26 and 25 not null eigenvalues.
 #lambda[1:26]
 
-
-#calcolo la distanza al quadrato
-alpha_Mahalanobis <- function(alpha,f1,f2,lambda, eigenft) {
-  t_points <- length(f1)
-  dis<-coeff<-prod<-rep(0,t_points)
-  
-  for (j in 1:t_points){
-    
-    coeff[j]<-lambda[j]/(lambda[j]+alpha)^2
-    prod[j]<-(scalar_prod(f1-f2,eigenft[,j]))^2
-    dis[j]<-coeff[j]*prod[j]
-    
-  }
-  res<-sum(dis)
-  return(res)
-}
-
-# approximation of the function f with f_alpha
-f_alpha_approx <-function(f,alpha,lambda,eigenft){
-  t_points <- length(f)
-  coeff<-prod<-res<-rep(0,t_points)
-  approx<-matrix(0,t_points,t_points)
-  
-  for (j in 1:t_points) {
-    
-    coeff[j]<-lambda[j]/(lambda[j]+alpha)
-    prod[j]<-scalar_prod(f,eigenft[,j])
-    approx[,j]<- as.numeric(coeff[j]*prod[j])*eigenft[,j]
-    res<-res+approx[,j]
-    
-  }
-  return(res)
-}
-
-# norm and inner product wrt sample covariance function K
-norm2_K <- function (f,lambda,eigenft){
-  t_points <- length(f)
-  norm_vect<-prod<-rep(0,t_points)
-  
-  for (j in 1:t_points){
-    prod[j]<-(scalar_prod(f,eigenft[,j]))^2
-    norm_vect[j]<-prod[j]/lambda[j]
-  }
-  
-  res<-sum(norm_vect)
-  return(res)
-}
-
-inner_product_K<- function(f,g,lambda,eigenft) {
-  t_points <- length(f)
-  prod_vect<-prod_f<-prod_g<-rep(0,t_points)
-  
-  for (j in 1:t_points){
-    prod_f[j]<-(scalar_prod(f,eigenft[,j]))
-    prod_g[j]<-(scalar_prod(g,eigenft[,j]))
-    
-    norm_vect[j]<-prod_f[j]*prod_g[j]/lambda[j]
-  }
-  
-  res<-sum(norm_vect)
-  return(res)
-}
-
 # prova dell'approssimazione con una funzione f presa dal dataset.
 f_prova <- f.data$ausxSL$data[1,]
 
@@ -177,7 +69,6 @@ lines(f.data$ausxSL$argvals, f_alpha_approx(f_prova,0.0001,lambda,eigenft), type
 
 
 #### alpha-Mahalanobis distance calculation ####
-
 # Smoothed data
 
 alpha <- 1e+4
@@ -198,6 +89,21 @@ for (i in 1:26){
 
 x11()
 image.plot(1:26,1:26,Mahalanobis_Distance)
+
+# Useless
+# # prova
+# kernel_estimator(f.data$ausxSL$data,X_bar,n,1,9,t_points) 
+# 
+# # Covariance matrix for the data
+# K_hat <- matrix(0,t_points,t_points)
+# for (i in 1:t_points){
+#   for (j in 1:t_points){
+#     K_hat[i,j] <- kernel_estimator(f.data$ausxSL$data,X_bar,n,i,j,t_points)
+#   }
+# }
+# # sub-matrix (5x5)
+# khat_example<-K_hat[1:5,1:5]
+# show(khat_example)
 
 ##### Save Workspace ####
 setwd("C:/Users/pietr/Desktop/Bayesian Statistics/Progetto/dati/BayesianProject")
