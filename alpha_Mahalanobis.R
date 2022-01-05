c54ffa16bf383209f995d479c65bb842fb466eb7
GiuliaRomani <giulia.romani@mail.polimi.it>
2022-01-05 13:46:55 +0100

    only real data

--- alpha_Mahalanobis.R
+++ alpha_Mahalanobis.R
@@ -1,23 +1,22 @@
+#### REAL DATA #### 
+
 #### Setup ####
 
 library(fda.usc)
 library(fda)
 library(fields)
 
-# Commentate i set delle directory non vostre
-setwd("C:/Users/pietr/Desktop/Bayesian Statistics/Progetto/dati")
-setwd('C:/Users/pietr/Desktop/Bayesian Statistics/Progetto/dati/potenziali_evocati')
+setwd("C:/Users/pietr/Desktop/Bayesian Statistics/Progetto/dati/BayesianProject")
 setwd("C:/Users/admin/Documents/R/Project_BS/BayesianProject") #GiuliaR
 
-load('functional_WP.RData')
+load("functional_WP.RData")
 load('Functions_WP.RData')
 
 
-#### Kernel estimator ####
+#### Application on real data ####
 n <- 26
 t_points <- 1600
 time<-seq(1,t_points)
-
 
 # Plot of functional mean vs data
 X_bar <- colMeans(f.data$ausxSL$data) # functional mean
@@ -70,7 +69,6 @@ lines(f.data$ausxSL$argvals, f_alpha_approx(f_prova,0.0001,lambda,eigenft), type
 
 #### alpha-Mahalanobis distance calculation ####
 # Smoothed data
-
 alpha <- 1e+4
 f.data_alpha <- matrix(0, nrow = 26, ncol = 1600)
 for (i in 1:26){
@@ -78,7 +76,6 @@ for (i in 1:26){
 }
 
 # alpha-Mahalanobis distance matrix 
-
 Mahalanobis_Distance <- matrix(0, nrow = 26, ncol = 26)
 for (i in 1:26){
   #print(i)
@@ -104,6 +101,81 @@ image.plot(1:26,1:26,Mahalanobis_Distance)
 # # sub-matrix (5x5)
 # khat_example<-K_hat[1:5,1:5]
 # show(khat_example)
+
+
+#### ORIGINAL CLUSTEING FUNCTIONS ####
+f.data.clust <- fda_clustering_mahalanobis(n_clust = 2, alpha = 10000,
+                                           cov_matrix = cov(f.data$ausxSL$data),
+                                           toll = 1e-2, data = f.data$ausxSL$data)
+c_opt <- f.data.clust$label
+show(c_opt)
+show(f.data.clust$loss)
+
+c1 <- f.data.clust$centroids[1,]
+c2 <- f.data.clust$centroids[2,]
+#c3 <- f.data.clust$centroids[3,]
+#c4 <- clust$centroids[4,]
+
+
+data1 <- f.data$ausxSL$data[which(c_opt=='1'),]
+data2 <- f.data$ausxSL$data[which(c_opt=='2'),]
+#data3 <- f.data$ausxSL$data[which(c_opt=='3'),]
+
+x11()
+par(mfrow = c(1,2))
+plot(time,f.data$ausxSL$data[1,], ylim = range(f.data$ausxSL$data) ,type = 'l', lwd = 2, main = "Data")
+for(i in 2:n){
+  lines(time,f.data$ausxSL$data[i,],type = 'l',lwd = 2)
+}
+
+plot(time,data1[1,], ylim = range(f.data$ausxSL$data) ,type = 'l', col = 'gold', lwd = 2, main = "Clustered data")
+for (i in 2:dim(data1)[1]){
+  lines(time,data1[i,],type = 'l', col = 'gold',lwd = 2)
+}
+for (i in 1:dim(data2)[1]){
+  lines(time,data2[i,],type = 'l', col = 'forestgreen',lwd = 2)
+}
+for (i in 1:dim(data3)[1]){
+  lines(time,data3[i,],type = 'l', col = 'gold',lwd = 2)
+}
+
+#### CLUSTERING FUNCTION UPDATING COVARIANCE WITHIN CLUSTERS ####
+f.data.clust <- fda_clustering_mahalanobis_updated(n_clust = 2, alpha = 10000,
+                                                   cov_matrix = cov(f.data$ausxSL$data),
+                                                   toll = 1e-2, data = f.data$ausxSL$data)
+
+c_opt <- f.data.clust$label
+show(c_opt)
+show(f.data.clust$loss)
+
+c1 <- f.data.clust$centroids[1,]
+c2 <- f.data.clust$centroids[2,]
+#c3 <- f.data.clust$centroids[3,]
+#c4 <- clust$centroids[4,]
+
+
+data1 <- f.data$ausxSL$data[which(c_opt=='1'),]
+data2 <- f.data$ausxSL$data[which(c_opt=='2'),]
+#data3 <- f.data$ausxSL$data[which(c_opt=='3'),]
+
+x11()
+par(mfrow = c(1,2))
+plot(time,f.data$ausxSL$data[1,], ylim = range(f.data$ausxSL$data) ,type = 'l', lwd = 2, main = "Data")
+for(i in 2:n){
+  lines(time,f.data$ausxSL$data[i,],type = 'l',lwd = 2)
+}
+
+plot(time,data1[1,], ylim = range(f.data$ausxSL$data) ,type = 'l', col = 'gold', lwd = 2, main = "Clustered data")
+for (i in 2:dim(data1)[1]){
+  lines(time,data1[i,],type = 'l', col = 'gold',lwd = 2)
+}
+for (i in 1:dim(data2)[1]){
+  lines(time,data2[i,],type = 'l', col = 'forestgreen',lwd = 2)
+}
+for (i in 1:dim(data3)[1]){
+  lines(time,data3[i,],type = 'l', col = 'gold',lwd = 2)
+}
+
 
 ##### Save Workspace ####
 setwd("C:/Users/pietr/Desktop/Bayesian Statistics/Progetto/dati/BayesianProject")
