dac0b7426ca6b9b140c650bb860175c1dc55b866
PietroSpina96 <pspina96@gmail.com>
2022-02-10 19:51:58 +0100

    real data

--- real_data.R
+++ real_data.R
@@ -50,7 +50,7 @@ image.plot(time,time,K_hat_R,main='R cov function')
 # Now the eigenvalues and eigenvectors of the K_hat_R matrix are called as the previous ones
 # so that we don't have to rewrite them in the code. The workspace is updated
 eigenvf<-eigen(K_hat_R)
-lambda<-eigenvf$values
+lambda1<-eigenvf$values
 eigenft<-eigenvf$vectors
 
 # OBS: from the theory we know that only (N-1) eigenvalues are not null where N=number of 
@@ -61,7 +61,7 @@ eigenft<-eigenvf$vectors
 f_prova <- f.data$ausxSL$data[1,]
 
 x11()
-plot(t(importMatrix(res, type = c('SL', 'sx'), position = 'au'))[1,], type = "l", ylim = c(-250, 250), lwd=2)
+plot(t(importMatrix(res, type = c('SL', 'sx'), position = 'au'))[1,], type = "l", ylim = c(-1000, 1000), lwd=2)
 lines(f.data$ausxSL$argvals, f_alpha_approx(f_prova,0.1,lambda,eigenft), type = 'l', lwd=2, col = 'firebrick2')
 lines(f.data$ausxSL$argvals, f_alpha_approx(f_prova,1e+4,lambda,eigenft), type = 'l', lwd=2, col = 'blue')
 lines(f.data$ausxSL$argvals, f_alpha_approx(f_prova,1e+5,lambda,eigenft), type = 'l', lwd=2, col = 'forestgreen')
@@ -69,7 +69,7 @@ lines(f.data$ausxSL$argvals, f_alpha_approx(f_prova,1e+5,lambda,eigenft), type =
 
 #### alpha-Mahalanobis distance calculation ####
 # Smoothed data
-alpha <- 1e+5
+alpha <- 0.1
 f.data_alpha <- matrix(0, nrow = 26, ncol = 1600)
 for (i in 1:26){
   f.data_alpha[i,] <- f_alpha_approx(f.data$ausxSL$data[i,],alpha,lambda,eigenft)
@@ -104,13 +104,54 @@ image.plot(1:26,1:26,Mahalanobis_Distance)
 # show(khat_example)
 
 
+#### Reduction of data points ####
+
+time_red <- seq(1,1600,3)
+f.data_red <- f.data$ausxSL$data[,time_red]
+f.Data <- list('data' = f.data_red, 'argvals' = time_red)
+
+x11()
+plot(f.Data$argvals,f.Data$data[1,], ylim = range(f.Data$data), type = 'l', lwd = 2, 
+     xlab = 'time', ylab = 'EVOKED PONTENTIAL')
+for (i in 2:26){
+  lines(f.Data$argvals,f.Data$data[i,], lwd = 2)
+}
+
+K_hat_t <- cov(f.Data$data)
+eigen_t <- eigen(K_hat)
+
+max(eigenvf$values) == max(eigen_t$values)
+
+
+#### Transformation of data ####
+
+f.data_t <- list('data' = f.data$ausxSL$data, 'argvals' = f.data$ausxSL$argvals)
+scale <- max(max(f.data_t$data),abs(min(f.data_t$data)))
+f.data_t$data <- f.data_t$data/scale
+
+x11()
+plot(f.data_t$argvals,f.data_t$data[1,], ylim = c(-1.05,1.05), type = 'l', lwd = 2, 
+     xlab = 'time', ylab = 'EVOKED PONTENTIAL')
+for (i in 2:26){
+  lines(f.data_t$argvals,f.data_t$data[i,], lwd = 2)
+}
+
+K_hat_t <- cov(f.data_t$data)
+eigen_t <- eigen(K_hat_R)
+lambda_t <- eigenvf$values
+max(lambda_t)
+eigenf_t <- eigenvf$vectors
+
+max(eigen_t$values) == max(eigenvf$values)
+
 #### Uniform prior with fixed covariance ####
 
 ##### k = 2 ######
 
-f.data_clust_2 <- fda_clustering_mahalanobis(n_clust = 2, alpha = alpha,
-                                           cov_matrix = cov(f.data$ausxSL$data),
-                                           toll = 1e-2, data = f.data$ausxSL$data)
+f.data_clust_2 <- fda_clustering_mahalanobis_general(n_clust = 2, alpha = alpha,
+                                                     cov_matrix = cov(f.Data$data),
+                                                     cov_type = 'fixed', toll = 1e-2, 
+                                                     data = f.Data$data)
 c_opt_2 <- f.data_clust_2$label
 show(c_opt_2)
 show(f.data_clust_2$loss)
@@ -118,29 +159,109 @@ show(f.data_clust_2$loss)
 c1 <- f.data_clust_2$centroids[1,]
 c2 <- f.data_clust_2$centroids[2,]
 
-data1 <- f.data$ausxSL$data[which(c_opt_2=='1'),]
-data2 <- f.data$ausxSL$data[which(c_opt_2=='2'),]
+data1 <- f.Data$data[which(c_opt_2=='1'),]
+data2 <- f.Data$data[which(c_opt_2=='2'),]
 
 ###### Plot #####
 x11()
 par(mfrow = c(1,2))
-plot(time,f.data$ausxSL$data[1,], ylim = range(f.data$ausxSL$data) ,type = 'l', lwd = 2, main = "Data")
+plot(f.Data$argvals,f.Data$data[1,], ylim = range(f.Data$data) ,type = 'l', lwd = 2, main = "Data")
 for(i in 2:n){
-  lines(time,f.data$ausxSL$data[i,],type = 'l',lwd = 2)
+  lines(f.Data$argvals,f.Data$data[i,],type = 'l',lwd = 2)
 }
 
-plot(time,data1[1,], ylim = range(f.data$ausxSL$data) ,type = 'l', col = 'gold', lwd = 2, main = "Uniform k = 2")
+plot(f.Data$argvals,data1[1,], ylim = range(f.Data$data) ,type = 'l', col = 'gold', lwd = 2, main = "Uniform (fixed cov) k = 2")
 for (i in 2:dim(data1)[1]){
-  lines(time,data1[i,],type = 'l', col = 'gold',lwd = 2)
+  lines(f.Data$argvals,data1[i,],type = 'l', col = 'gold',lwd = 2)
 }
 for (i in 1:dim(data2)[1]){
-  lines(time,data2[i,],type = 'l', col = 'forestgreen',lwd = 2)
+  lines(f.Data$argvals,data2[i,],type = 'l', col = 'forestgreen',lwd = 2)
 }
+lines(f.Data$argvals, c1, lwd = 3)
+lines(f.Data$argvals, c2, lwd = 3)
+
 
 ##### k = 3 ######
-f.data_clust_3 <- fda_clustering_mahalanobis(n_clust = 3, alpha = alpha,
-                                             cov_matrix = cov(f.data$ausxSL$data),
-                                             toll = 1e-2, data = f.data$ausxSL$data)
+f.data_clust_3 <- fda_clustering_mahalanobis_general(n_clust = 3, alpha = alpha,
+                                                     cov_matrix = cov(f.Data$data),
+                                                     cov_type = 'fixed', toll = 1e-2, 
+                                                     data = f.Data$data)
+c_opt_3 <- f.data_clust_3$label
+show(c_opt_3)
+show(f.data_clust_3$loss)
+
+c1 <- f.data_clust_3$centroids[1,]
+c2 <- f.data_clust_3$centroids[2,]
+c3 <- f.data_clust_3$centroids[3,]
+
+data1 <- f.Data$data[which(c_opt_3=='1'),]
+data2 <- f.Data$data[which(c_opt_3=='2'),]
+data3 <- f.Data$data[which(c_opt_3=='3'),]
+
+###### Plot #####
+x11()
+par(mfrow = c(1,2))
+plot(f.Data$argvals,f.Data$data[1,], ylim = range(f.Data$data) ,type = 'l', lwd = 2, main = "Data")
+for(i in 2:n){
+  lines(f.Data$argvals,f.Data$data[i,],type = 'l',lwd = 2)
+}
+
+plot(f.Data$argvals,data1, ylim = range(f.Data$data) ,type = 'l', col = 'gold', lwd = 2, main = "Uniform (fixed cov) k = 3")
+# plot(f.Data$argvals,data1[1,], ylim = range(f.Data$data) ,type = 'l', col = 'gold', lwd = 2, main = "Uniform (fixed cov) k = 3")
+for (i in 2:dim(data1)[1]){
+  lines(f.Data$argvals,data1[i,],type = 'l', col = 'gold',lwd = 2)
+}
+for (i in 1:dim(data2)[1]){
+  lines(f.Data$argvals,data2[i,],type = 'l', col = 'forestgreen',lwd = 2)
+}
+for (i in 1:dim(data3)[1]){
+  lines(f.Data$argvals,data3[i,],type = 'l', col = 'firebrick3',lwd = 2)
+}
+lines(f.Data$argvals, c1, lwd = 3)
+lines(f.Data$argvals, c2, lwd = 3)
+lines(f.Data$argvals, c3, lwd = 3)
+
+#### Uniform prior with covariance updating within clusters ####
+
+##### k = 2 ######
+
+f.data_clust_2up <- fda_clustering_mahalanobis_general(n_clust = 2, alpha = alpha,
+                                                     cov_matrix = cov(f.Data$data),
+                                                     cov_type = 'updated', toll = 1e-2, 
+                                                     data = f.Data$data)
+c_opt_2 <- f.data_clust_2up$label
+show(c_opt_2)
+show(f.data_clust_2up$loss)
+
+c1 <- f.data_clust_2up$centroids[1,]
+c2 <- f.data_clust_2up$centroids[2,]
+
+data1 <- f.Data$data[which(c_opt_2=='1'),]
+data2 <- f.Data$data[which(c_opt_2=='2'),]
+
+###### Plot #####
+x11()
+par(mfrow = c(1,2))
+plot(f.Data$argvals,f.Data$data[1,], ylim = range(f.Data$data) ,type = 'l', lwd = 2, main = "Data")
+for(i in 2:n){
+  lines(f.Data$argvals,f.Data$data[i,],type = 'l',lwd = 2)
+}
+
+plot(f.Data$argvals,data1[1,], ylim = range(f.Data$data) ,type = 'l', col = 'gold', lwd = 2, main = "Uniform (fixed cov) k = 2")
+for (i in 2:dim(data1)[1]){
+  lines(f.Data$argvals,data1[i,],type = 'l', col = 'gold',lwd = 2)
+}
+for (i in 1:dim(data2)[1]){
+  lines(f.Data$argvals,data2[i,],type = 'l', col = 'forestgreen',lwd = 2)
+}
+lines(f.Data$argvals, c1, lwd = 3)
+lines(f.Data$argvals, c2, lwd = 3)
+
+##### k = 3 ######
+f.data_clust_3 <- fda_clustering_mahalanobis_general(n_clust = 2, alpha = alpha,
+                                                     cov_matrix = cov(f.data$ausxSL$data),
+                                                     cov_type = 'updated', toll = 1e-2, 
+                                                     data = f.data$ausxSL$data)
 c_opt_3 <- f.data_clust_3$label
 show(c_opt_3)
 show(f.data_clust_3$loss)
@@ -173,7 +294,15 @@ for (i in 1:dim(data3)[1]){
 }
 
 
-#### Pitman-Yor Prior with fixed covariance ####
+
+
+
+
+
+
+
+
+#### Pitman-Yor EPPF Prior ####
 
 alpha <- alpha
 sigma <- 0.25
