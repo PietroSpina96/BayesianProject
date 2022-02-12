4907159641166521650a74cebb269aa8114497e5
mttVsc <matteo1.vescovi@mail.polimi.it>
2022-02-13 00:52:55 +0100

    updated covariance each allocation

--- gibbs_sampling_RealData.R
+++ gibbs_sampling_RealData.R
@@ -1,3 +1,16 @@
+# queste sinceramente non credo servano ma boh ne dubbio
+library(fda.usc)
+library(fda)
+library(fields)
+
+load("functional_WP.RData")
+load('Functions_WP.RData')
+
+time_red <- seq(1,1600,3)
+f.data_red <- f.data$ausxSL$data[,time_red]
+f.Data <- list('data' = f.data_red, 'argvals' = time_red)
+rm(time_red)
+rm(f.data_red)
 
 discrepancy_within <- function(x, centroid, alpha, eig){
   #compute square discrepancy
@@ -15,8 +28,7 @@ discrepancy_within <- function(x, centroid, alpha, eig){
 }
 
 
-
-prob_i_k <- function(obs_index, clust_index, Ci, data, lambda, alpha, eig, verbose=FALSE){
+prob_i_k <- function(obs_index, clust_index, Ci, data, lambda, alpha, verbose=FALSE){
   ## compute fullconditional term for PY-EPPF prior
   
   
@@ -29,10 +41,14 @@ prob_i_k <- function(obs_index, clust_index, Ci, data, lambda, alpha, eig, verbo
   }
   
   x_k = data[idx_k,]
-  if(length(idx_k)>1)
+  
+  #update covariance eigs
+  eig = eigen(cov(x_k))
+  
+  if(length(idx_k)>1) #i.e. at least 2 observations
     centroid = colMeans(x_k)
   else
-    centroid = x_k
+    return(-1) # if it's the only observation it can't be reallocated
   
   
   start_time <- Sys.time()
@@ -42,17 +58,16 @@ prob_i_k <- function(obs_index, clust_index, Ci, data, lambda, alpha, eig, verbo
   if(verbose){
     print(paste("Computing full discrepancy in ",end_time - start_time,"s"))
   }
-  # print(paste("numerosità x_k = ",length(idx_k)))
-  # print(paste("osservazione rimossa = ",obs_idx))
-  # print(paste(" numerosità X_k_no_i = ", dim(x_k_no_i)[1]))
-  if(length(idx_k)>1){
-    x_k_no_i = x_k[-obs_idx,]
-    if(length(idx_k) > 2){
-      #i.e. if x_k_no_i has 1 row
-      centroid_no_i = colMeans(x_k_no_i)
-    }else{
-      centroid_no_i = x_k_no_i
+  
+  x_k_no_i = x_k[-obs_idx,]
+  
+  if(length(idx_k)==2){ # i.e. exactly 2 obs in the cluster
+    disc_no_i <- 0
+    if(verbose){
+      print("Since length(idx_k)==2 computation of discrepancy without obs is not necessary! ")
     }
+  }else{
+    centroid_no_i = colMeans(x_k_no_i)
     start_time <- Sys.time()
     disc_no_i <- discrepancy_within(x_k_no_i, centroid_no_i, alpha, eig)
     end_time <- Sys.time()
@@ -60,26 +75,26 @@ prob_i_k <- function(obs_index, clust_index, Ci, data, lambda, alpha, eig, verbo
     if(verbose){
       print(paste("Computing discrepancy without obs ",obs_index," in ",end_time - start_time,"s"))
     }
-    return(exp(-lambda*(disc_in - disc_no_i)))
-  }else{
-    disc_no_i <- 0
-    
-    if(verbose){
-      print("Cluster had only 1 observation, disc_no_i set to zero")
-    }
-    
-    return(exp(-lambda*(disc_in - disc_no_i))) # approx to be checked with mario
   }
+  return(exp(-lambda*(disc_in - disc_no_i)))
 }
 
 
-update_k_i <- function(obs_idx, Ci, n_clust, data, lambda, alpha, eig){
+
+update_k_i <- function(obs_idx, Ci, n_clust, data, lambda, alpha, verbose=F){
   # Sample a value for the cluster, compatible with the given expression of the full conditional
   probs = rep(0,n_clust)
-  for(ii in 1:n_clust)
-    probs[ii] = prob_i_k(obs_idx, ii, Ci, data, lambda, alpha, eig,verbose=F) #FOR UNIFORM
+  for(ii in 1:n_clust){
+    val = prob_i_k(obs_idx, ii, Ci, data, lambda, alpha, verbose=F)
+    if(val == -1){
+      if(verbose)
+        print("Atomic cluster detected!")
+      return(ii) #if it's the only obs in that cluster it is not reallocated
+    }else{
+      probs[ii] = val
+    }
+  }
   
-  #print(paste("Probabilità di allocazione elem #",obs_idx,": C1:",probs[1]," C2:",probs[2]))
   k = sample(n_clust, 1, replace = TRUE, prob = probs)
   return(k)
 }
@@ -89,14 +104,17 @@ update_k_i <- function(obs_idx, Ci, n_clust, data, lambda, alpha, eig){
 
 #### MH for our distro
 
-gibbs_sampler_uniform <- function(N, N_burnin, x0, data, lambda, alpha, eig, verbose = F){
+gibbs_sampler <- function(N, N_burnin, x0, data, lambda, alpha, k, verbose = F){
   #compute MH samples of the posterior for cluter indexes Ci
   #INPUTS
   # N = number of samples
-  # N_burnin = number of initial elements to discard
+  # N_burnin = number of initial elements of the chain to be discarded
   # x0 = initial labels
+  # data = functional data [number of observations * number of points of a single observation ]
+  # lambda = weigth of the likelihood wrt the prior
+  # alpha = smoothing factor
+  # k = number of cluster
   
-  k = 2 #FIXED HERE FOR NOW
   n <- dim(data)[1]
   
   samplee = matrix(0, nrow = N, ncol = n)
@@ -114,22 +132,13 @@ gibbs_sampler_uniform <- function(N, N_burnin, x0, data, lambda, alpha, eig, ver
     
     gibbs_time_init <- Sys.time()
     
-    # for(t in 1:n){ #update one component at the time
-    #   #sample the value according to the full conditional
-    #   init_time <- Sys.time()
-    #   X_temp[t] = update_k_i(t, X_temp, k, data, sig, lambda, alpha, eig)
-    #   finish_time <- Sys.time()
-    #   print(paste("Sampled indicator for obs",t," in ",finish_time - init_time,"s"))
-    # }
-    
     for(t in 1:n)#update one component at the time
-      X_temp[t] = update_k_i(t, X_temp, k, data, lambda, alpha, eig)
+      X_temp[t] = update_k_i(t, X_temp, k, data, lambda, alpha, verbose)
     
     if(iterMH > N_burnin){
       posz = iterMH - N_burnin
       samplee[posz,] = X_temp
     }
-    
     
     gibbs_time_finish <-Sys.time()
     
@@ -141,78 +150,16 @@ gibbs_sampler_uniform <- function(N, N_burnin, x0, data, lambda, alpha, eig, ver
   return(samplee)
 }
 
-##### data import
+#RUN DI PROVA
+Ci <- gibbs_sampler(2, 1, c_opt_2, f.Data$data, 1, 1e4, 2, verbose = T)
 
-library(fda.usc)
-library(fda)
-library(fields)
+#VERA RUN
 
-load("functional_WP.RData")
-load('Functions_WP.RData')
+Ci <- gibbs_sampler(5000, 1000, c_opt_2, f.Data$data, 1, 1e4, 2, verbose = T)
 
-time_red <- seq(1,1600,3)
-f.data_red <- f.data$ausxSL$data[,time_red]
-f.Data <- list('data' = f.data_red, 'argvals' = time_red)
-rm(time_red)
-rm(f.data_red)
+#SAVE RESULTS
+
+save(Ci, file="indexes_Ci_uniform_lambda1_alpha1e4_k2_realData.RData")
 
 
 
-Ci <- gibbs_sampler_uniform(5, 1, c_opt_2, f.Data$data, 1, 1e4 , eigen(cov(f.Data$data)), verbose = T)
-
-
-
-#actual run
-
-Ci <- gibbs_sampler_uniform(10000, 1000, c_opt_2, f.Data$data, 1, 1e4 , eigen(cov(f.Data$data)), verbose = T)
-
-
-
-save(Ci, file="indexes_Ci_uniform_lambda1_realData.RData")
-
-save.image("chain_Ci_data1_uniform_realData.RData")
-
-#### MCMC interpretation
-
-df <- list()
-for(ii in 1:dim(Ci)[1]){
-  df[[ii]] <- Ci[ii,]
-}
-
-
-S=similarityMat(df)
-
-x11()
-heatmap(S)
-
-BL1=minbinder(S, cls=Ci, method = "avg",
-              max.k = NULL, include.lg = FALSE, start.cl = NULL, tol = 0.001)
-BL1$cl
-
-binder(Ci,S)
-
-
-BL2=cluster_est_binder(data.frame(t(Ci)),log(S))
-
-BL2$c_est
-
-
-library(NPflow)
-library(mcclust)
-
-S2=similarityMat(data.frame(t(Ci)))
-
-x11()
-heatmap(S2, keep.dendro=FALSE)
-
-
-BL1=minbinder(S2, cls=Ci, method = "avg",
-              max.k = NULL, include.lg = FALSE, start.cl = NULL, tol = 0.001)
-BL1$cl
-
-binder(Ci,S2)
-
-
-BL2=cluster_est_binder(data.frame(t(Ci)),log(S2))
-
-BL2$c_est
